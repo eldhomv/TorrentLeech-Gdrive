ef23d88f4cc9a0404dcb9867ce4d4edbd53e7d0e
eldhomv <64636539+eldhomv@users.noreply.github.com>
2021-01-04 16:19:57 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,26 +1,18 @@
 import os
 from pathlib import Path
 
-class Config:
-    
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = os.environ.get('API_HASH')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    SESSION_NAME = os.environ.get('SESSION_NAME', ':memory:')
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL'))
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
-    MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
-    MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 600))
-    TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
-    SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 5))
-    HOST = os.environ.get('HOST', '')
-    TIMEOUT = int(os.environ.get('TIMEOUT', 60 * 30))
-    DEBUG = bool(os.environ.get('DEBUG'))
-    
-    SCRST_OP_FLDR = Path('screenshots/')
-    SMPL_OP_FLDR = Path('samples/')
-    THUMB_OP_FLDR = Path('thumbnails/')
-    COLORS = ['white', 'black', 'red', 'blue', 'green', 'yellow', 'orange', 'purple', 'brown', 'gold', 'silver', 'pink']
-    FONT_SIZES_NAME = ['Small', 'Medium', 'Large']
-    FONT_SIZES = [30, 40, 50]
+class Config(Config):
+    TG_BOT_TOKEN= "1301781981:AAGb2NngTvx_sd76flP9RDs4x1KnS9MfSj8" #imp
+    APP_ID = 1711488 #imp
+    API_HASH = "707667e743148abde192655aa64d21f6" #imp
+    AUTH_CHANNEL = [-420813515, 999162609] #imp replace your_id with your id from telegram or delete
+    GLEECH_COMMAND = "gleech"
+    YTDL_COMMAND = 'ytdl'
+    TELEGRAM_LEECH_COMMAND_G = "tleech"
+    CLONE_COMMAND_G = "gclone"
+    PYTDL_COMMAND_G = "pytdl"
+    DESTINATION_FOLDER = "my drive"
+    INDEX_LINK = "https://www.emvsplex.ga/0:"
+    LEECH_COMMAND = "leech"
+    RCLONE_CONFIG = """type = drive\nclient_id = 689614248022-f8jm4n8os876g0fe3k66hu4vn2hjnmm6.apps.googleusercontent.com\nclient_secret = OCz4MamcnnHh70oanlxxMoeK\nscope = drive\ntoken = {"access_token":"ya29.a0AfH6SMCs-sXEWJS9isl61E_eYmpNxAR7l2w2JXb91HhHwjKdbXyG8ajmHEpDwjTrlopRySglAmyqJxgm6DBBom1KAphXFV0Z32b_Kt1K9dJJMWZa9Kn07ksbzT4PJqga2qMSBOIJli2kiCX_Gt2NUGlWl-fauqh15kOxbmNxmmk","token_type":"Bearer","refresh_token":"1//0g1wyhUv0ZH3CCgYIARAAGBASNwF-L9Irpe0OOfycW5ELFb2PO_Bp2ln9SE2voqWQHwDyTXBxRsj1m--tQS6KNMOgZXx-uAUSjyk","expiry":"2020-12-23T10:13:03.7093259+05:30"}\nteam_drive = 0AL2f4TZ1lf0IUk9PVA"""
+    #put your config(replacing new lines with \n) in triple quote like above: """<your one liner config>"""
